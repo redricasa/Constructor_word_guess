96c29e39d2c45458f7d22e7e189f80a7590fcf6d
redricasa <fredeb26@gmail.com>
2019-10-08 22:06:40 -0700

    cut/paste word constructor from index.js to word.js

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 
-function Word(word) {
-    // word =  'hello'
-    // word.split('') > ['h','e','l','l','o']
-
-    this.letters = word.split('').map(function(letter) {
-        return new Letter(letter)
-    })
-
-}
 
---
+++ word.js
@@ -0,0 +1,9 @@
+function Word(word) {
+    // word =  'hello'
+    // word.split('') > ['h','e','l','l','o']
+
+    this.letters = word.split('').map(function(letter) {
+        return new Letter(letter)
+    })
+
+}
